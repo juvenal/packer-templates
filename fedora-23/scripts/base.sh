f658b59827dc7b193fde1a9626a103a16d323cc2
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2016-05-15 21:36:17 -0300

    Ease support for Fedora 23
    This commit cleans the Fedora 23 build scripts and template.

--- fedora-23/scripts/base.sh
+++ fedora-23/scripts/base.sh
@@ -1,5 +1,3 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 dnf -y update
 dnf -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
-dnf -y install gcc-go gcc-objc gcc-objc++ gcc-gfortran
-dnf -y install llvm llvm-libs llvm-doc llvm-static clang
